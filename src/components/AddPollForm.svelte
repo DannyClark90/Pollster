134cba4c68fc0256c3e34268ec474595e0ecc729
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-10-30 21:33:17 +0000

    Custom form validation added.

--- src/components/AddPollForm.svelte
+++ src/components/AddPollForm.svelte
@@ -1,12 +1,62 @@
 <script>
   import Button from './Button.svelte'
+
+  let formData = {
+    question: '',
+    answerA: '',
+    answerB: '',
+  }
+
+  let errors = {
+    question: '',
+    answerA: '',
+    answerB: '',
+  }
+
+  let valid = false
+
+  const handleSubmit = () => {
+    valid = true
+
+    // Check question validity
+    if (formData.question.trim().length < 5) {
+      valid = false
+      errors.question = 'Question must be at least 5 characters long'
+    } else {
+      errors.question = ''
+    }
+    // Check answer A validity
+    if (formData.answerA.trim().length < 1) {
+      valid = false
+      errors.answerA = 'Answer A must not be empty'
+    } else {
+      errors.answerA = ''
+    }
+
+    // Check answer B validity
+    if (formData.answerB.trim().length < 1) {
+      valid = false
+      errors.answerB = 'Answer B must not be empty'
+    } else {
+      errors.answerB = ''
+    }
+
+    // Add new poll
+    if(valid){
+      console.log('valid', formData);
+    }
+
+    if(!valid){
+      console.log('invalid', errors);
+    }
+  }
 </script>
 
 <div class="absolute z-10 flex w-full flex-col items-center top-[19rem] gap-16">
-  <h1 class="font-h1 text-lg-h1 tracking-lg-h1">Add New Poll</h1>
+  <h1 class="font-h1 text-sm-h1 tracking-lg-h1">Add New Poll</h1>
   <form
-    action=""
     class="z-10 flex xs:w-full md:w-[75%] lg:w-[60%] flex-col gap-10 px-20"
+    on:submit|preventDefault={handleSubmit}
   >
     <div class="flex flex-col gap-1">
       <label for="question" class="font-h2 text-sm-h2">Poll Question:</label>
@@ -15,6 +65,7 @@
         type="text"
         placeholder="enter question..."
         class="rounded-lg drop-shadow-md h-16 pl-5"
+        bind:value={formData.question}
       />
     </div>
 
@@ -25,6 +76,7 @@
         type="text"
         placeholder="enter option A..."
         class="rounded-lg drop-shadow-md h-16 pl-5"
+        bind:value={formData.answerA}
       />
     </div>
 
@@ -35,9 +87,10 @@
         type="text"
         placeholder="enter option B..."
         class="rounded-lg drop-shadow-md h-16 pl-5"
+        bind:value={formData.answerB}
       />
     </div>
-    
+
     <div class="flex justify-center">
       <Button buttonText={'Submit Poll'} />
     </div>
