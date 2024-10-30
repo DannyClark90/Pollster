3f2bd12c8f2416830a5238c5a74735485a2939df
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-10-30 10:57:37 +0000

    Button animation added.

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -2,6 +2,6 @@
     export let buttonText;
 </script>
 
-<button class="bg-secondary drop-shadow-md px-6 py-4 rounded-xl" >
+<button class="bg-secondary drop-shadow-md px-6 py-4 rounded-xl hover:-translate-y-1 active:translate-y-[0.1rem] active:shadow-inner" >
     <h2 class="font-h2 text-sm-h2 text-white">{buttonText}</h2>
 </button>
