39bfa00fd04c417fedc334a361459efb9794c4b7
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-10-30 10:47:58 +0000

    Poll card & button created.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,34 @@
 <script>
   import Header from './components/Header.svelte'
   import Footer from './components/Footer.svelte'
+  import PollCard from './components/PollCard.svelte';
+ 
+  let currentPolls = [
+    {
+      title: "Favorite Fruit",
+      answerA: { answer: "Apple", votes: 40 },
+      answerB: { answer: "Banana", votes: 25 },
+      totalVotes: 65
+    },
+    {
+      title: "Favorite Fruit",
+      answerA: { answer: "Apple", votes: 40 },
+      answerB: { answer: "Banana", votes: 25 },
+      totalVotes: 65
+    }]
 </script>
 
 <style>
 </style>
 
 <Header />
-<main>
-  <div class="xxs:bg-center bg-pattern w-lvw h-lvh"></div>
+<main class="z-0 xxs:bg-center bg-pattern w-lvw h-lvh relative">
+  
+  <div class="absolute z-10 flex w-full flex-col items-center top-[18rem] gap-10">
+    <h1 class="font-h1 text-sm-h1 tracking-lg-h1">Current Polls</h1>
+    {#each currentPolls as poll, index (index)}
+      <PollCard {poll}/>
+    {/each}
+  </div>
 </main>
 <Footer />
---
+++ src/components/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let buttonText;
+</script>
+
+<button class="bg-secondary drop-shadow-md px-6 py-4 rounded-xl" >
+    <h2 class="font-h2 text-sm-h2 text-white">{buttonText}</h2>
+</button>
